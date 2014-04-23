b506d7843324e618c59d7aabc309ee256c11677b
Imran Chowdhury <ich@vizrt.com>
2014-04-23 12:36:16 +0600

    Adding new script to connect to hudson automatically

---
+++ TestAutomation/ContentStudio/ConnectToHudson/ConnectToHudson.bat
@@ -0,0 +1,3 @@
+@echo off
+call C:\Users\Imran\Sikuli-IDE-1.0.0-Win32\sikuli-ide.cmd -r C:\Users\Imran\Desktop\ConnectToHudson.sikuli
+exit
---
+++ TestAutomation/ContentStudio/ConnectToHudson/ConnectToHudson.sikuli/ConnectToHudson.py
@@ -0,0 +1,8 @@
+from sikuli.Sikuli import *
+wait("HudsonConnectWarning.png", 300)
+if exists("HudsonConnectWarning.png"):
+    click("Run.png")
+    
+    
+    
+    
