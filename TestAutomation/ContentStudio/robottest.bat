dd7f69220a29d4f691b1d7b0e394cc8d1c838ec7
Imran Chowdhury <ich@vizrt.com>
2015-03-06 14:08:09 +0600

    Bug fix

--- TestAutomation/ContentStudio/robottest.bat
+++ TestAutomation/ContentStudio/robottest.bat
@@ -3,7 +3,7 @@
 set sikuli_jar=sikuli-script.jar
 set robotframework_jar=C:\Test-automation\ContentStudio\robotframework-2.8.1.jar
  
-jre6/bin/java -cp "robotframework-2.8.1.jar;%sikuli_jar%" ^
+jre6\bin\java -cp "robotframework-2.8.1.jar;%sikuli_jar%" ^
 -Dpython.path="%robotframework_jar%\Lib";"%sikuli_jar%\Lib" ^
 org.robotframework.RobotFramework ^
 --pythonpath=content-studio.sikuli ^
