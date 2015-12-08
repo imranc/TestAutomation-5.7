5168faae8ae299934b2abdef0f2efe5f4bcef9ae
sja <sja@vizrt.com>
2015-12-08 11:52:38 +0600

    WF-Bug fix in 1.9.9 - ece-sikuli.py

--- TestAutomation/ContentStudio/content-studio.sikuli/eceSikuli.py
+++ TestAutomation/ContentStudio/content-studio.sikuli/eceSikuli.py
@@ -75,7 +75,19 @@ class eceSikuli(object):
                 elif args[0] == 'UP':
                         type(Key.UP)
 
-
+		def type_key_if_exists(self, *args):
+                
+                checkElementExists = args[0]
+                checkElementExistsImageName = checkElementExists+'.png'
+                if exists(Pattern(checkElementExistsImageName).similar(0.99)):
+                        if len(args) == 2:
+                                eceSikuli.type_key(self, args[1])
+                        elif len(args) == 3:
+                                eceSikuli.type_key(self, args[1], args[2])
+                        
+                        else:
+                                raise AssertionError("Image name and keys must be passed as argument.")
+                wait(2)
         def click_element_if_exists(self, *args):
                 checkElementExists = args[0]
                 checkElementExistsImageName = checkElementExists+'.png'
